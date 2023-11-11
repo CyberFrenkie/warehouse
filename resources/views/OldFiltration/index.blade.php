c4dcd37390ca948ce3cc00defdb4ea90a64bf9d3
CyberFrenky <vitezkojampek@gmail.com>
2023-11-11 17:22:42 +0100

    update

---
+++ resources/views/OldFiltration/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layout')
+
+
+@section('content')
+    <div class="of-wrapper">
+        <div class="heading col-12">
+            <h1>Old filtration</h1>
+        </div>
+        
+        <div class="levels">
+            <ul class="level-list">
+                
+                <a href="/of/second" class="list-item-link">
+                    <li class="list-item">Pumps</li>
+                </a>
+
+                <a href="/of/first" class="list-item-link">
+                    <li class="list-item">Pipelines</li>
+                </a>
+
+            </ul>
+        </div>
+    </div>
+    
+    
+@endsection
---
+++ resources/views/PumpStations/pump-station-db.blade.php
@@ -0,0 +1,25 @@
+@extends('layout')
+
+
+@section('content')
+    <div class="of-wrapper">
+        <div class="heading col-12">
+            <h1>Pumpna stanica Debeli lug</h1>
+        </div>
+        
+        <div class="levels">
+            <ul class="level-list">
+                
+                <a href="/of/second" class="list-item-link">
+                    <li class="list-item">Pumps</li>
+                </a>
+
+                <a href="/of/first" class="list-item-link">
+                    <li class="list-item">Pipelines</li>
+                </a>
+            </ul>
+        </div>
+    </div>
+    
+    
+@endsection
